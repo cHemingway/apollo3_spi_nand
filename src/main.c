298464982c7c4073e59e0be7bdec483a23c7c351
Chris Hemingway <chris.hemingway@gmail.com>
2019-12-22 16:24:46 +0000

    Dhara: read/write test, not yet working

--- src/main.c
+++ src/main.c
@@ -26,7 +26,8 @@
 #define TEST_PROGRAM    true
 #endif
 
-uint8_t page_buf[PAGE_SIZE];
+uint8_t dhara_page_buf[PAGE_SIZE];
+uint8_t dhara_test_buf[PAGE_SIZE];
 
 // Override am_print_string so hard_fault_handler outputs over RTT
 void am_print_string(char *pcStr) {
@@ -40,6 +41,34 @@ void SysTick_Handler(void)
 {
     g_tick_ms++;
 }
+
+
+// Forked versions of dhara/tests/util.c to not use abort()
+void seq_gen(unsigned int seed, uint8_t *buf, size_t length)
+{
+	size_t i;
+	srand(seed);
+	for (i = 0; i < length; i++)
+		buf[i] = rand();
+}
+
+void seq_assert(unsigned int seed, const uint8_t *buf, size_t length)
+{
+	size_t i;
+
+	srand(seed);
+	for (i = 0; i < length; i++) {
+		const uint8_t expect = rand();
+
+		if (buf[i] != expect) {
+			fprintf(stderr, "seq_assert: mismatch at %ld in "
+				"sequence %d: 0x%02x (expected 0x%02x)\n",
+				i, seed, buf[i], expect);
+			while(1); // Changed from abort()
+		}
+	}
+}
+
 
 //*****************************************************************************
 // Main
@@ -99,15 +128,28 @@ int main(void)
 
     // DHARA FS Map Tests
     // From dhara/tests/journal.c
+    #if TEST_DHARA
     {
         printf("Running dhara tests.. \n");
         struct dhara_map map;
         dhara_error_t dhara_err;
+        // Number of sectors to test. Too many makes logic analyser decode crash
+        const unsigned test_sectors = 3;   
+
         printf("Map init\n");
-        dhara_map_init(&map, &dhara_adaptor_nand, page_buf, GC_RATIO);
+        dhara_map_init(&map, &dhara_adaptor_nand, dhara_page_buf, GC_RATIO);
         dhara_map_resume(&map, NULL);
         printf("  capacity: %d\n", dhara_map_capacity(&map));
         printf("\n");
+
+        printf("Writing %u sectors \n", test_sectors);
+        for (int i=0; i<test_sectors; i++) {
+            seq_gen(i, dhara_test_buf, PAGE_SIZE);
+            if (dhara_map_write(&map, i, dhara_test_buf, &dhara_err) < 0) {
+		        printf("map_write error: %s \n", dhara_strerror(dhara_err));
+                return; // TODO exit()
+            }
+        }
 
         printf("Sync...\n");
         retcode = dhara_map_sync(&map, &dhara_err);
@@ -115,17 +157,31 @@ int main(void)
             printf("dhara_map_sync error %s \n",dhara_strerror(dhara_err));
         }
         printf("Resume...\n");
-        dhara_map_init(&map, &dhara_adaptor_nand, page_buf, GC_RATIO);
+        dhara_map_init(&map, &dhara_adaptor_nand, dhara_page_buf, GC_RATIO);
         retcode = dhara_map_resume(&map, &dhara_err);
         if (retcode) {
             printf("dhara_map_resume error %s \n",dhara_strerror(dhara_err));
         }
+        printf("  capacity: %d\n", dhara_map_capacity(&map));
+	    printf("  use count: %d\n", dhara_map_size(&map));
 
+        printf("Read back...\n");
+	    for (int i = 0; i < test_sectors; i++) {
+
+            if(dhara_map_read(&map, i, dhara_test_buf, &dhara_err)) {
+                printf("map_read error: %s \n", dhara_strerror(dhara_err));
+                return; // TODO exit()
+            }
+		    seq_assert(i, dhara_test_buf, PAGE_SIZE);
+        }
+        printf("Success! /n");
         while(1);
     }
+    #endif
+
     
 
-    while(1) {
+    while (1) {
         retcode = nand_test(TEST_BLOCK, TEST_PROGRAM);
         if (retcode == AM_HAL_STATUS_SUCCESS) {
             printf("FLASH TEST PASS \n\n");
